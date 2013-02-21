655061422b3773af74e60d88b0134d826279ca17
John Chase <chasejohn@gmail.com>
2013-02-20 20:08:55 -0600

    initial version

---
+++ TwitterFinder/TwitterFinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterFinder
+//
+//  Created by John Chase on 2/20/13.
+//  Copyright (c) 2013 John Chase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCCAppDelegate class]));
+    }
+}
